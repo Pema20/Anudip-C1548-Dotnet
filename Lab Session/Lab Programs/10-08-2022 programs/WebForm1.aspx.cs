5dea1de900212be592652ed8b66885cf93522e80
Pema20 <88247554+Pema20@users.noreply.github.com>
2022-08-09 21:20:09 +0530

    Add files via upload

---
+++ Lab Session/Lab Programs/10-08-2022 programs/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            int res = int.Parse(TextBox1.Text) / int.Parse(TextBox2.Text);
+
+
+
+
+            TextBox3.Text = res.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int res=int.Parse(TextBox1.Text) + int.Parse(TextBox2.Text);
+
+           
+
+            
+            TextBox3.Text = res.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int res = int.Parse(TextBox1.Text) - int.Parse(TextBox2.Text);
+
+
+
+
+            TextBox3.Text = res.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            int res = int.Parse(TextBox1.Text) * int.Parse(TextBox2.Text);
+
+
+
+
+            TextBox3.Text = res.ToString();
+        }
+    }
+}
